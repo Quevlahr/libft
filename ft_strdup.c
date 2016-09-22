02c0cb7674ed57d92da77c47254ad7784e9a9935
Quentin Roulon <quroulon@student.42.fr>
2016-09-22 19:04:30 +0200

    strdup utilise directement ft_strnew

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: quroulon <quroulon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/25 17:40:01 by quroulon          #+#    #+#             */
-/*   Updated: 2016/05/03 16:13:00 by quroulon         ###   ########.fr       */
+/*   Updated: 2016/09/22 17:35:24 by quroulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,7 @@ char	*ft_strdup(const char *s1)
 	char			*s2;
 
 	i = 0;
-	if (!(s2 = (char*)malloc(sizeof(char) * ft_strlen(s1))))
-		return (NULL);
+	s2 = ft_strnew(ft_strlen(s1));
 	while (i < ft_strlen(s1))
 	{
 		s2[i] = ((char*)s1)[i];
