5c9a408a1ee63620c7d2160ff0a2c2e419271773
Quentin Roulon <quroulon@student.42.fr>
2016-10-12 12:22:58 +0200

    upd : get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,86 @@
 /*   By: quroulon <quroulon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/12 12:00:59 by quroulon          #+#    #+#             */
-/*   Updated: 2016/10/07 16:49:44 by quroulon         ###   ########.fr       */
+/*   Updated: 2016/10/09 20:17:52 by quroulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+// static int			ft_read(int const fd, char **str, char **line, int res)
+// {
+// 	char			*buf;
+// 	int				i;
+
+// 	buf = ft_strnew(BUFF_SIZE);
+// 	while ((res = (read(fd, buf, BUFF_SIZE) > 0)))
+// 	{
+// 		i = 0;
+// 		while (buf[i] && buf[i] != '\n')
+// 			i++;
+// 		if (buf[i] == '\n')
+// 		{
+// 			*line = (*line == NULL) ? ft_strsub(buf, 0, i) : ft_strjoin(*line, ft_strsub(buf, 0, i));
+// 			*str = ft_strsub(buf, ++i, ft_strlen(buf));
+// 			if ((*str)[0] == '\0')
+// 				ft_strdel(str);
+// 			return (1);
+// 		}
+// 		else
+// 		{
+// 			if (*line == NULL)
+// 				*line = ft_strnew(0);
+// 			*line = ft_strjoin(*line, buf);
+// 		}
+// 	}
+// 	if (res == -1)
+// 		return (-1);
+// 	if (res == 0 && *line == NULL && *str == NULL)
+// 		return (0);
+// 	if (res == 0 && *line == NULL)
+// 	{
+// 		*line = ft_strnew(0);
+// 		*line = ft_strjoin(*line, *str);
+// 		ft_strdel(str);
+// 		return (1);
+// 	}
+// 	return (1);
+// }
+
+// int					get_next_line(int const fd, char **line)
+// {
+// 	int				i;
+// 	static char		*str = NULL;
+
+// 	i = 0;
+// 	*line = NULL;
+// 	if (line == NULL)
+// 		return (-1);
+// 	if (str != NULL)
+// 	{
+// 		while (str[i])
+// 		{
+// 			if (str[i] == '\n')
+// 			{
+// 				*line = ft_strjoin(*line, ft_strsub(str, 0, i));
+// 				str = ft_strsub(str, i, ft_strlen(str));
+// 				return (1);
+// 			}
+// 			i++;
+// 		}
+// 		*line = ft_strnew(0);
+// 		*line = ft_strjoin(*line, str);
+// 		ft_strdel(&str);
+// 	}
+// 	return (ft_read(fd, &str, line, 0));
+// }
+
+
+
+
+
+
+
 
 static char			*ft_strsub_gnl(char const *s, unsigned int start, size_t len)
 {
